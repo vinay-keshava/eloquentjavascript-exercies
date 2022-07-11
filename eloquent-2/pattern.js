3419c82f1bccb82726e44013279f25531b23fedb
vinay-keshava <vinaykeshava@disroot.org>
2022-07-12 00:11:32 +0530

    02 Program Structure

---
+++ eloquent-2/pattern.js
@@ -0,0 +1,14 @@
+var a="#";
+for(var i=0;i<8;i++){
+if(i%2==0)
+			process.stdout.write(" ");
+	for(var j=0;j<8;j++)
+{
+		if(j%2==0)
+			process.stdout.write(a+" ");
+		else
+			process.stdout.write("");
+	}
+
+console.log();
+}
---
+++ eloquent-2/sample.js
@@ -0,0 +1,7 @@
+var a="#";
+for(var i=0;i<8;i++){
+	if(i%2==0)
+		console.log(" ",a," ",a," ",a," ",a);
+	else
+		console.log(a," ",a," ",a," ",a);
+}
